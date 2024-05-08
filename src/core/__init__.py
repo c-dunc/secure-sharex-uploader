6b6d8f5e2c7c74b7d573118fa9824b55359f6623
Callum Duncan <callumdunc@gmail.com>
2024-05-08 23:37:15 +0100

    config restructure

--- src/core/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-routes = Blueprint('routes', __name__)
-
-# import here
-from .routes import *
--- src/core/configuration.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-def load(path='../config.json'):
-    config_path = os.path.join(os.path.dirname(__file__), path)
-    with open(config_path, 'r', errors='ignore', encoding='UTF-8') as file:
-        config = json.load(file)
-    return config
