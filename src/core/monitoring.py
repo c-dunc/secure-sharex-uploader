a158922001784c331db631e5f62c2d136e9b56d3
Callum Duncan <callumdunc@gmail.com>
2024-05-09 01:06:35 +0100

    vulnerablity data added

---
+++ src/core/monitoring.py
@@ -0,0 +1,6 @@
+class Vulnerabilities:
+    COMMON_SECRETS = ('secret', 'key', 'mySecret')
+    COMMON_PATHS = ('', 'i', 'ss', 'file', 'files', 'upload')
+    COMMON_ENDPOINTS = ('upload.php', 'up.php', 'sharex.php', 'file.php', 'files.php', 'fileupload.php', 'image.php')
+    COMMON_FIELD_NAMES = ('secret', 'key', 'apiKey', 'apikey', 'token', 'api_key')
+    COMMON_FORM_NAMES = ('sharex', 'file', 'files', 'image')
